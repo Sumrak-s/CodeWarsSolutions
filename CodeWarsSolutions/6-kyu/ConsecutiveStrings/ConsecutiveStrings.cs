14b21aaa0e3078afb292ffea9b9c079480bad5a7
Sergei Gololobov <sumrak.sergei@gmail.com>
2022-07-05 00:29:46 +0300

    Consecutive strings

---
+++ CodeWarsSolutions/6-kyu/ConsecutiveStrings/ConsecutiveStrings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsSolutions
+{
+    /*
+    Title: Consecutive strings
+
+    Description: You are given an array(list) strarr of strings and an integer k. 
+    Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+    Kata Link:
+    https://www.codewars.com/kata/56a5d994ac971f1ac500003e/train/csharp
+    */
+
+    public class ConsecutiveStrings
+    {
+        public static string LongestConsec(string[] strarr, int k)
+        {
+            if (strarr.Length < k) return "";
+
+            string longestString = "";
+
+            for (int i = 0; i <= strarr.Length - k; i++)
+            {
+                var currentString = "";
+                for (int y = i; y < i + k; y++) currentString += strarr[y];
+                if (longestString.Length < currentString.Length) longestString = currentString;
+            }
+
+            return longestString;
+        }
+    }
+}
---
+++ CodeWarsTests/6-kyu/ConsecutiveStringsTest/ConsecutiveStringsTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests
+{
+    internal class ConsecutiveStringsTest
+    {
+        private static void testing(string actual, string expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public static void test1()
+        {
+            Console.WriteLine("Basic Tests");
+            testing(ConsecutiveStrings.LongestConsec(new String[] { "zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail" }, 2), "abigailtheta");
+            testing(ConsecutiveStrings.LongestConsec(new String[] { "ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh" }, 1), "oocccffuucccjjjkkkjyyyeehh");
+            testing(ConsecutiveStrings.LongestConsec(new String[] { }, 3), "");
+            testing(ConsecutiveStrings.LongestConsec(new String[] { "itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck" }, 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
+            testing(ConsecutiveStrings.LongestConsec(new String[] { "wlwsasphmxx", "owiaxujylentrklctozmymu", "wpgozvxxiu" }, 2), "wlwsasphmxxowiaxujylentrklctozmymu");
+            testing(ConsecutiveStrings.LongestConsec(new String[] { "zone", "abigail", "theta", "form", "libe", "zas" }, -2), "");
+            testing(ConsecutiveStrings.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 3), "ixoyx3452zzzzzzzzzzzz");
+            testing(ConsecutiveStrings.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 15), "");
+            testing(ConsecutiveStrings.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 0), "");
+        }
+    }
+}

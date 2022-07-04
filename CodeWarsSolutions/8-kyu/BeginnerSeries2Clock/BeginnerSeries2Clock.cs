10210f9b6eb691f97f91ac0ebd0c1f45ce0d2165
Sergei Gololobov <sumrak.sergei@gmail.com>
2022-07-04 08:17:51 +0300

    Beginner Series #2 Clock

---
+++ CodeWarsSolutions/8-kyu/BeginnerSeries2Clock/BeginnerSeries2Clock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsSolutions
+{
+    /*
+
+     Title: Beginner Series #2 Clock
+
+     Description:
+     Clock shows h hours, m minutes and s seconds after midnight.
+     Your task is to write a function which returns the time since midnight in milliseconds.
+
+     Kata Link:
+     https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/csharp
+
+     */
+    public class BeginnerSeries2Clock
+    {
+        public static int Past(int h, int m, int s)
+        {
+            int result = 0;
+
+            if (h != 0 || h < 0) result += h * 3600;
+            if (m != 0 || m < 0) result += m * 60;
+            if (s >= 0) result += s;
+
+            return result * 1000;
+        }
+    }
+}
---
+++ CodeWarsTests/BeginnerSeries2Clock/BeginnerSeries2ClockTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests
+{
+    internal class BeginnerSeries2ClockTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(61000, BeginnerSeries2Clock.Past(0, 1, 1));
+        }
+    }
+}

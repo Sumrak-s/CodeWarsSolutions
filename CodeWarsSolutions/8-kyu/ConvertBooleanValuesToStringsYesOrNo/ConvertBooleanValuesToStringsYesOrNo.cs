7b866c7774dcd463f656e702b8e34ceca32fc489
Sergei Gololobov <sumrak.sergei@gmail.com>
2022-07-04 08:44:23 +0300

    Convert boolean values to strings 'Yes' or 'No'.

---
+++ CodeWarsSolutions/8-kyu/ConvertBooleanValuesToStringsYesOrNo/ConvertBooleanValuesToStringsYesOrNo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsSolutions
+{
+    public class ConvertBooleanValuesToStringsYesOrNo
+    {
+
+        /*
+        Title: Convert boolean values to strings 'Yes' or 'No'.
+
+        Description:
+        Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+        Kata Link:
+        https://www.codewars.com/kata/53369039d7ab3ac506000467/train/csharp
+        */
+
+        public static string boolToWord(bool word)
+        {
+            return word ? "Yes" : "No";
+        }
+    }
+}
---
+++ CodeWarsTests/ConvertBooleanValuesToStringsYesOrNoTest/ConvertBooleanValuesToStringsYesOrNoTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests
+{
+    internal class ConvertBooleanValuesToStringsYesOrNoTest
+    {
+        [Test]
+        public void boolToWordReturned1()
+        {
+            Assert.AreEqual("Yes", ConvertBooleanValuesToStringsYesOrNo.boolToWord(true));
+            Console.WriteLine("Expected Yes");
+        }
+
+        [Test]
+        public void boolToWordReturned2()
+        {
+            Assert.AreEqual("No", ConvertBooleanValuesToStringsYesOrNo.boolToWord(false));
+            Console.WriteLine("Expected No");
+        }
+
+        [Test]
+        public void boolToWordReturned3()
+        {
+            Assert.AreEqual("Yes", ConvertBooleanValuesToStringsYesOrNo.boolToWord(true));
+            Console.WriteLine("Expected Yes");
+        }
+    }
+}

03771eda3ea366bb37e7e77c5f3c998752fbe7ed
Sergei Gololobov <sumrak.sergei@gmail.com>
2022-07-04 08:35:27 +0300

    MakeUpperCase

---
+++ CodeWarsSolutions/8-kyu/MakeUpperCase/MakeUpperCase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsSolutions
+{
+    public class MakeUpperCase
+    {
+        /*
+
+        Title: MakeUpperCase
+
+        Description:
+        Write a function which converts the input string to uppercase.
+
+        Kata Link:
+        https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/csharp
+
+        */
+
+        public static string Solution(string str)
+        {
+            return str.ToUpper();
+        }
+    }
+}
---
+++ CodeWarsTests/MakeUpperCaseTest/MakeUpperCaseTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests
+{
+    internal class MakeUpperCaseTest
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual("HELLO", MakeUpperCase.Solution("hello"));
+        }
+    }
+}

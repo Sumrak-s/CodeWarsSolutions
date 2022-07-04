e4adb39f5f14993698f4650dc92c1b8aea12d36d
Sergei Gololobov <sumrak.sergei@gmail.com>
2022-07-04 10:14:21 +0300

    Who likes it?

---
+++ CodeWarsSolutions/6-kyu/WhoLikesIt/WhoLikesIt.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsSolutions
+{
+    /*
+    Title: Who likes it?
+
+    Description: You probably know the "like" system from Facebook and other pages. 
+    People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+    Implement the function which takes an array containing the names of people that like an item.
+    It must return the display text as shown in the examples:
+
+    []                                -->  "no one likes this"
+    ["Peter"]                         -->  "Peter likes this"
+    ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+    ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+    ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+
+    Kata Link:
+    https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/csharp
+    */
+
+    public class WhoLikesIt
+    {
+        public static string Likes(string[] name)
+        {
+            return name.Length switch
+            {
+                0 => "no one likes this",
+                1 => $"{name[0]} likes this",
+                2 => $"{name[0]} and {name[1]} like this",
+                3 => $"{name[0]}, {name[1]} and {name[2]} like this",
+                _ => $"{name[0]}, {name[1]} and {name.Length - 2} others like this"
+            };
+        }
+    }
+}

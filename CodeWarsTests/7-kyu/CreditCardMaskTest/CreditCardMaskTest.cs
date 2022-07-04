6a6adafc797e5fd6da5bcafdeca248dc1909234e
Sergei Gololobov <sumrak.sergei@gmail.com>
2022-07-04 10:25:30 +0300

    Credit Card Mask

---
+++ CodeWarsTests/7-kyu/CreditCardMaskTest/CreditCardMaskTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests
+{
+    internal class CreditCardMaskTest
+    {
+        [Test]
+        public void ExamplesTests()
+        {
+            Assert.AreEqual("############5616", CreditCardMask.Maskify("4556364607935616"));
+            Assert.AreEqual("1", CreditCardMask.Maskify("1"));
+            Assert.AreEqual("#1111", CreditCardMask.Maskify("11111"));
+        }
+    }
+}
